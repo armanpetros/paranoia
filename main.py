296fe5ef2e88b136817fecf89c02726ecd320239
dkyopwa <nedvedv@gmail.com>
2021-12-09 21:44:59 +0300

    Hi

--- main.py
+++ main.py
@@ -1,49 +1,43 @@
-#!/usr/bin/python
-import re
-def paranoyc():
-    while True:
-        name = str(input("Enter name: "))
-        if "Stop" in (" " + name.upper() + " ") or "stop" in (" " + name.lower() + " "):
-            break
-        if not name.isalpha() or re.search(r'[^-a-zA-Z-а-яА-ЯёЁ]', name):
-            print("Can use only latin and cyrillic symbols. Please retry!")
-        else:
-            while True:
-                try:
-                    v1 = int(input("enter number v1: "))
-                except ValueError:
-                    print("Can use only whole numbers. Please retry!")
-                else:
-                    if v1 < 0:
-                        print("Can use numbers above zero. Please retry!")
-                    else:
-                        while True:
-                            try:
-                                v2 = int(input("Enter number v2: "))
-                            except ValueError:
-                                print("Can use only whole numbers. Please retry!")
-                            else:
-                                if v2 < 0:
-                                    print("Can use numbers above zero. Please retry!")
-                                else:
-                                    while True:
-                                        try:
-                                            v3 = float(input("Enter number v3: "))
-                                        except ValueError:
-                                            print("Can use only numbers. Please retry!")
-                                        else:
-                                            if v3 == 0:
-                                                print("Can use numbers above zero. Please retry!")
-                                            else:
-                                                print(f"{name}, hi!")
-                                                v1 = int(f"{v1}" * 3)
-                                                v2 = int(f"{v2}" * 2)
-                                                v3 = float(f"{v3}")
-                                                s = (v1 + v2) / v3
-                                                print("Your sum = " + "%.2f" % s)
-                                                break
-                                    break
-                        break
-            break
+#!/usr/bin/env python
+# nevl 2021
 
-paranoyc()
+def is_number(data, type, check_zero=False):
+    try:
+        if (check_zero and type(data) == 0) or \
+                (type == int and data.find('.') >= 0):
+            raise ZeroDivisionError
+        return type(data)
+    except:
+        return None
+
+def input_number(type, text, check_zero=False):
+    while True:
+        data = is_number(input(text), type, check_zero)
+        if data is None:
+            print(f'Enter number in {type} format. {"" if not check_zero else "Not zero."}')
+        else:
+            return data
+
+if __name__ == '__main__':
+    while True:
+        # fetch name
+        while True:
+            name = input('Enter name: ')
+            strings = name.split()
+            flag = strings[0].isalpha() and True if len(strings) < 2 else strings[1].isalpha()
+            if not flag or name.count(' ') > 1:
+                print('Not a name. Enter only letters and one space.')
+                print(name.count(' '), not name.isalpha())
+            else:
+                break
+
+        # check name
+        if name.lower() == 'stop':
+            quit(0)
+
+        # fetch numbers
+        number1 = input_number(int, 'Enter number 1: ')
+        number2 = input_number(int, 'Enter number 2: ')
+        number3 = input_number(float, 'Enter number 3: ', check_zero=True)
+
+        print('{}, hi!\nSum = {}'.format(name.title(), (int(f'{number1}'*3) + int(f'{number2}'*2)) / float(number3)))
